56e4237b954e76af044db513adabce5204a5561c
samuel.casanova <samuel.casanova@immfly.com>
2025-09-29 16:49:56 +0200

    add a first nodeJs script

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 docker exec -it mongodb mongosh mongodb://admin:password@localhost:27017 --file /src/initial_setup_and_aggregate.js
 docker exec -it mongodb mongosh mongodb://admin:password@localhost:27017 --file /src/schema_validation.js
+
+node ./src/app.js
---
+++ src/app.js
@@ -0,0 +1,28 @@
+// Require MongoDB language driver
+const { MongoClient } = require('mongodb')
+
+// Set the value of uri to your Atlas connection string.
+const uri = 'mongodb://admin:password@localhost:27017'
+
+// Create the MongoClient instance
+const client = new MongoClient(uri)
+
+// Establishes a connection to the database using the MongoClient instance
+const main = async () => {
+  try {
+    await client.connect()
+    console.log('Connected to MongoDB Atlas!')
+    // list out all the databases in the cluster
+    const dbs = await client.db().admin().listDatabases()
+    console.table(dbs.databases)
+  } catch (error) {
+    console.error(error)
+  } finally {
+    await client.close()
+  }
+}
+
+// Run the main function, catch any errors and finally close the connection when the main function is done
+main()
+  .catch((err) => console.log(err))
+  .finally(() => client.close())
