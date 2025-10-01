86da80e9461c9102bce77ef65df7314562e991bb
samuel.casanova <samuel.casanova@immfly.com>
2025-10-01 18:47:59 +0200

    add updateOne push operation to script

--- src/04.replacing_documents.js
+++ src/04.replacing_documents.js
@@ -22,9 +22,20 @@ const reviewR2 = db.reviews.findOne({ review_id: 'r2' })
 printjson(reviewR2)
 
 console.log('Updating Frankenstein book price to 9.99:')
-const updateResult = db.books.updateOne({ _id: '0000800883' }, { $set: { amount: 9.99 } })
-printjson(updateResult)
+const updateSetResult = db.books.updateOne({ _id: '0000800883' }, { $set: { amount: 9.99 } })
+printjson(updateSetResult)
 
 console.log('Querying Frankenstein book after price update:')
 const frankensteinBook = db.books.findOne({ _id: '0000800883' })
 printjson(frankensteinBook)
+
+console.log('Adding a new alias to "Michael Crichton":')
+const updatePushResult = db.authors.updateOne(
+  { _id: 22 },
+  { $push: { aliases: { $each: ['Mike', 'Mikey'] } } }
+)
+printjson(updatePushResult)
+
+console.log('Querying "Michael Crichton" author after adding new alias:')
+const michaelCrichton = db.authors.findOne({ _id: 22 })
+printjson(michaelCrichton)
