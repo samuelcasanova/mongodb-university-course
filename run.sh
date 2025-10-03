02e1652916814aa298e0061a0e24241969bfd60b
samuel.casanova <samuel.casanova@immfly.com>
2025-10-03 09:11:13 +0200

    add advanced queries

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ docker exec -it mongodb mongosh mongodb://admin:password@localhost:27017 --file 
 docker exec -it mongodb mongosh mongodb://admin:password@localhost:27017 --file /src/02.schema_validation.js
 docker exec -it mongodb mongosh mongodb://admin:password@localhost:27017 --file /src/03.retrieving_documents.js
 docker exec -it mongodb mongosh mongodb://admin:password@localhost:27017 --file /src/04.replacing_documents.js
-docker exec -it mongodb mongosh mongodb://admin:password@localhost:27017 --file /src/05.deleting_documents.js
+docker exec -it mongodb mongosh mongodb://admin:password@localhost:27017 --file /src/05.advanced_querying_documents.js
+#docker exec -it mongodb mongosh mongodb://admin:password@localhost:27017 --file /src/06.deleting_documents.js
---
+++ src/05.advanced_querying_documents.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-undef */
+console.log('Books published after 1920, sorted by year descending, limited to 3:')
+const cursor = db.books.find({ year: { $gt: 1920 } }, { title: 1, year: 1, _id: 0 }).sort({ year: -1 }).limit(3) // -1 for descending order, 1 for ascending order
+printjson(cursor.toArray())
